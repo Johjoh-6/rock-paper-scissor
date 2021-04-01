bb1fac42a821ed67d16e936db418b5414427c561
Johjoh-6 <six.johann@gmail.com>
2021-04-01 10:03:22 -0400

    Advance on JS and fixe html

--- main.js
+++ main.js
@@ -4,25 +4,31 @@ playerScore.dataset.score = 1;
 const robotScore = document.querySelector('#robot');
 robotScore.dataset.score= 3;
 
-// button
-// const rock
+// button player
+const rockButton = document.querySelector('#rock');
+const paperButton = document.querySelector('#paper');
+const scissorButton = document.querySelector('#scissor');
+
+// div robot for after playing event
+const rockDiv = document.querySelector('#rock-fixe');
+const paperDiv = document.querySelector('#paper-fixe');
+const scissorDiv = document.querySelector('#scissor-fixe');
+
 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection == "rock" && comuputerSelection =="paper"){
-        return "You Lose! Paper beats Rock";
-    } if (playerSelection == "paper" && comuputerSelection =="scissor"){
-        return "You Lose! Scissor beats Paper";
-    } if (playerSelection == "scissor" && comuputerSelection =="rock"){
-        return "You Lose! Rock beats Scissor";
-    } 
-    if (playerSelection == "scissor" && comuputerSelection =="paper"){
-        return "You win!  Scissor beats Paper";
-}if (playerSelection == "rock" && comuputerSelection =="scissor"){
-    return "You win! Rock beats Scissor";
-} if (playerSelection == "paper" && comuputerSelection =="rock"){
-    return "You win! Paper beats Rock";
-} 
+    if (
+        (playerSelection === "rock" && comuputerSelection === "paper") || 
+        (playerSelection === "paper" && comuputerSelection === "scissor") ||
+        (playerSelection === "scissor" && comuputerSelection === "rock") ||
+        (robotScore <= 5 || playerScore <= 5)) {
+            // you lose
+        }
+    if (
+        (playerSelection == "scissor" && comuputerSelection =="paper") ||
+        (playerSelection == "rock" && comuputerSelection =="scissor") ||
+        (playerSelection == "paper" && comuputerSelection =="rock") ||
+        (robotScore <= 5 || playerScore <= 5)) {
+            // you win 
+        }
+
 }
-const playerSelection = document.querySelector(button);
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
