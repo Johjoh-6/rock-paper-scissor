436ec73292a02565a08304fbb18fe950c69ca8d9
Johjoh-6 <six.johann@gmail.com>
2021-04-01 15:00:54 -0400

    update HTML and css, new function botPlay and ShinyBot

--- main.js
+++ main.js
@@ -7,28 +7,57 @@ robotScore.dataset.score= 3;
 // button player
 const rockButton = document.querySelector('#rock');
 const paperButton = document.querySelector('#paper');
-const scissorButton = document.querySelector('#scissor');
+const scissorsButton = document.querySelector('#scissors');
 
 // div robot for after playing event
 const rockDiv = document.querySelector('#rock-fixe');
 const paperDiv = document.querySelector('#paper-fixe');
-const scissorDiv = document.querySelector('#scissor-fixe');
+const scissorsDiv = document.querySelector('#scissors-fixe');
 
-
-function playRound(playerSelection, computerSelection) {
-    if (
-        (playerSelection === "rock" && comuputerSelection === "paper") || 
-        (playerSelection === "paper" && comuputerSelection === "scissor") ||
-        (playerSelection === "scissor" && comuputerSelection === "rock") ||
-        (robotScore <= 5 || playerScore <= 5)) {
-            // you lose
-        }
-    if (
-        (playerSelection == "scissor" && comuputerSelection =="paper") ||
-        (playerSelection == "rock" && comuputerSelection =="scissor") ||
-        (playerSelection == "paper" && comuputerSelection =="rock") ||
-        (robotScore <= 5 || playerScore <= 5)) {
-            // you win 
-        }
-
+// Function for draw an random play from the bot
+function botPlay() {
+    const number = Math.floor(Math.random() * 1000);
+    if (number % 3 === 0) {
+        return 'rock';
+    } if (number % 3 === 1) {
+        return 'paper';
+    } else {
+            return 'scissor';
+    }
 }
+console.log(botPlay());
+//function for shiny div of the robot
+function shinyBot(botSelection) {
+    if (botSelection === 'rock') {
+        rockDiv.classList.add('selected');
+    } if (botSelection === 'paper') {
+        paperDiv.classList.add('selected');
+    } if (botSelection === 'scisssor') {
+        scissorsDiv.classList.add('selected');
+    }
+}
+ //clear the style
+function removeShinyBot(botSelection) {
+    rockDiv.classList.remove('selected');
+    paperDiv.classList.remove('selected');
+    scissorsDiv.classList.remove('selected');
+}
+
+
+// function playRound(playerSelection, computerSelection) {
+//     if (
+//         (playerSelection === "rock" && comuputerSelection === "paper") || 
+//         (playerSelection === "paper" && comuputerSelection === "scissor") ||
+//         (playerSelection === "scissor" && comuputerSelection === "rock") ||
+//         (robotScore <= 5 || playerScore <= 5)) {
+//             // you lose
+//         }
+//     if (
+//         (playerSelection == "scissor" && comuputerSelection =="paper") ||
+//         (playerSelection == "rock" && comuputerSelection =="scissor") ||
+//         (playerSelection == "paper" && comuputerSelection =="rock") ||
+//         (robotScore <= 5 || playerScore <= 5)) {
+//             // you win 
+//         }
+
+// }
